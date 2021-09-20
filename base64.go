c021f614e4b1f48fa3f365794969fc39b78a29a8
Michael OReilly <77720629+oreillymj@users.noreply.github.com>
2021-09-20 13:04:07 +0100

    Update base64.go

--- base64.go
+++ base64.go
@@ -1,15 +1,3 @@
-// https://gobyexample.com/base64-encoding
-// https://gobyexample.com/command-line-arguments
-// https://gobyexample.com/if-else
-// https://www.base64decode.org/
-
-// https://blog.filippo.io/shrink-your-go-binaries-with-this-one-weird-trick/
-// go build -ldflags="-s -w" base64.go
-// https://github.com/xaionaro/documentation/blob/master/golang/reduce-binary-size.md
-// go build -a -gcflags=all="-l -B -wb=false" -ldflags="-w -s"; stat -c %s base64.go
-// UPX https://softfamous.com/upx/
-// https://www.cockroachlabs.com/blog/go-file-size/
-
 package main
 
 import (
